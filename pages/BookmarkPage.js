f691f1a9fc2ac106b10ed164d12686d0b2fd2acd
GyeongHo <ludeng0727@gmail.com>
2023-11-18 20:35:18 +0900

    feat BookmarkPage

--- pages/BookmarkPage.js
+++ pages/BookmarkPage.js
@@ -1,16 +1,40 @@
-import React, {useEffect} from 'react';
+import React, {useState} from 'react';
 import {useFocusEffect} from '@react-navigation/native';
+import userService from '../service/UserService';
+import {Button, FlatList, SafeAreaView, View} from 'react-native';
+import {styles} from '../styles/MainPageStyle';
+import RenderItem from '../components/Common/RenderItem';
+import WelfareContents from '../components/Common/WelfareContents';
 
 const BookmarkPage = ({navigation, token}) => {
+  const [welfareList, setWelfareList] = useState([]);
+  const [bookmarkList, setBookmarkList] = useState([]);
+
   useFocusEffect(
     React.useCallback(() => {
-      if (!token) {
+      if (token) {
+        userService.getUserInfo(token).then(user => {
+          setBookmarkList(user.bookmarks);
+          setWelfareList(user.bookmarks.map(bookmark => bookmark.welfare));
+        });
+      } else {
         navigation.navigate('Login');
       }
     }, [navigation, token]),
   );
 
-  return <></>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <WelfareContents
+        welfareList={welfareList}
+        bookmarkList={bookmarkList}
+        setWelfareList={setWelfareList}
+        setBookmarkList={setBookmarkList}
+        token={token}
+        navigation={navigation}
+      />
+    </SafeAreaView>
+  );
 };
 
 export default BookmarkPage;
